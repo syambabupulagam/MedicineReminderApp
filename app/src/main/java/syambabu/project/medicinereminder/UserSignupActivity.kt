7f01a25bf77869172344a8de97c4c6405e1cf46a
syambabupulagam <syambabupulagam@gmail.com>
2025-11-08 01:09:59 +0530

    Designed User Registration Screen

---
+++ app/src/main/java/syambabu/project/medicinereminder/UserSignupActivity.kt
@@ -0,0 +1,13 @@
+package syambabu.project.medicinereminder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
